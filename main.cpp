50f0bb609596388a8b06910820aaab2873a1109f
Lorenzocricri <91842642+Lorenzocricri@users.noreply.github.com>
2021-11-09 23:07:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
-   return 0;
+  int n=0,primo=0,secondo=1,risultato=1;
+    cin >> n;
+        for(int i=1;i<=n;i++){
+            if(risultato<=n) {
+                cout << risultato << endl;
+                risultato = primo + secondo;
+                primo = secondo;
+                secondo = risultato;
+            }else{
+                return 0;
+            }
+        }
+    return 0;
 }
